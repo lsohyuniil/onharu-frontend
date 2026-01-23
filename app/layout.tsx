86cc36b8c306d893eaf89980e1c7da5828393cca
yeaseula <kminea@naver.com>
2026-01-23 20:23:43 +0900

    ✨ Feat: footer 레이아웃 컴포넌트 제작

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
+import { Footer } from "@/components/layout/Footer";
 
 const pretendard = localFont({
   src: "../fonts/pretendard/PretendardVariable.woff2",
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={pretendard.variable}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
