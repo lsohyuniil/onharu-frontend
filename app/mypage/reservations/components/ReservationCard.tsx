acc85cff9f40154a89f7acdc78e18e64bc5341b7
lsohyuniil <lim07so10h@gmail.com>
2026-02-02 01:10:48 +0900

    feat: 예약 내역 표시 ReservationCard UI 컴포넌트 구현

---
+++ app/mypage/reservations/components/ReservationCard.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/Button";
+import StatusBadge from "./StatusBadge";
+import ReservationActionButtons from "./ReservationActionButtons";
+import { ReservationItem } from "../types";
+
+export default function ReservationCard({
+  id,
+  role,
+  status,
+  title,
+  reservationDate,
+  reservationTime,
+  people,
+  address,
+  cancelReason,
+}: ReservationItem) {
+  return (
+    <li className="bg-secondary mb-2 flex flex-col gap-3 rounded-[10px] p-3 sm:mb-5 sm:flex-row sm:items-center sm:justify-between sm:p-6">
+      <div className="flex flex-col gap-1">
+        <div className="flex items-center gap-1">
+          <StatusBadge status={status} />
+
+          <h3 className="text-sm font-bold sm:text-base">{title}</h3>
+
+          <Button varient="dark" width="xs" height="xs" fontSize="sm">
+            채팅하기
+          </Button>
+        </div>
+
+        <p className="text-xs font-medium sm:text-sm">
+          {reservationDate} · {reservationTime} · {people}인 예약
+        </p>
+
+        {address && <p className="text-text-secondary text-xs font-medium sm:text-sm">{address}</p>}
+
+        {cancelReason && (
+          <p className="text-xs font-medium sm:text-sm">취소 사유 : {cancelReason}</p>
+        )}
+      </div>
+
+      <div className="flex w-full min-w-25 gap-1 sm:w-auto sm:flex-col sm:justify-end">
+        <ReservationActionButtons
+          role={role}
+          status={status}
+          reservationId={id}
+          reservationDate={reservationDate}
+        />
+      </div>
+    </li>
+  );
+}
