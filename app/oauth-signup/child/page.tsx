2aee05db3a6531bd7cd4e03f3b217a580ebfe3d4
lsohyuniil <lim07so10h@gmail.com>
2026-02-09 18:30:28 +0900

    feat: OAuth 회원가입 추가 정보 입력 페이지 추가

---
+++ app/oauth-signup/child/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { SignupFormValues } from "@/app/signup/types";
+import DocumentUploadField from "@/components/feature/DocumentUploadField";
+import { FIELD_CONFIG } from "@/components/form-fields/fieldConfig";
+import { FormField } from "@/components/form-fields/FormField";
+import FormLayout from "@/components/layout/FormLayout";
+import { Button } from "@/components/ui/Button";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function OauthChildSignupPage() {
+  const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<SignupFormValues>({ mode: "onSubmit" });
+
+  const handleFilesChange = (files: File[]) => setUploadedFiles(files);
+
+  const onSubmit = (data: SignupFormValues) => {
+    const finalData = {
+      ...data,
+      documents: uploadedFiles,
+    };
+
+    console.log("회원가입 데이터:", finalData);
+  };
+
+  return (
+    <div className="mt-section-sm-top wrapper md:mt-section-lg-top mb-section-sm-bottom md:mb-section-lg-bottom flex min-h-screen items-center justify-center">
+      <div className="w-full max-w-115">
+        <FormLayout title="아동 회원가입">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+            {/* 닉네임 */}
+            <FormField<SignupFormValues>
+              name="nickname"
+              config={FIELD_CONFIG.nickname}
+              register={register}
+              errors={errors}
+            />
+            {/* 증명 서류 */}
+            <div className="flex flex-col">
+              <div className="sm:text-md mb-1.25 text-base font-medium sm:mb-2.5">
+                증명서류 <span className="text-danger ml-1">*</span>
+              </div>
+              <DocumentUploadField
+                register={register}
+                errors={errors}
+                watch={watch}
+                onFilesChange={handleFilesChange}
+              />
+            </div>
+
+            <div className="mt-2.5 sm:mt-7.5">
+              <Button type="submit" varient="default" width="lg" height="md" fontSize="md">
+                회원가입
+              </Button>
+            </div>
+          </form>
+        </FormLayout>
+      </div>
+    </div>
+  );
+}
---
+++ app/oauth-signup/owner/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import BusinessNumberField from "@/app/signup/components/fields/BusinessNumberField";
+import { SignupFormValues } from "@/app/signup/types";
+import { FIELD_CONFIG } from "@/components/form-fields/fieldConfig";
+import { FormField } from "@/components/form-fields/FormField";
+import FormLayout from "@/components/layout/FormLayout";
+import { Button } from "@/components/ui/Button";
+import { useForm } from "react-hook-form";
+
+export default function OauthOwnerSignupPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+  } = useForm<SignupFormValues>({ mode: "onSubmit" });
+
+  const onSubmit = (data: SignupFormValues) => {
+    console.log("회원가입 데이터:", data);
+  };
+
+  return (
+    <div className="mt-section-sm-top wrapper md:mt-section-lg-top mb-section-sm-bottom md:mb-section-lg-bottom flex min-h-screen items-center justify-center">
+      <div className="w-full max-w-115">
+        <FormLayout title="매장 회원가입">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+            {/* 매장명 */}
+            <FormField<SignupFormValues>
+              name="storeName"
+              config={FIELD_CONFIG.storeName}
+              register={register}
+              errors={errors}
+            />
+
+            {/* 사업자등록번호 */}
+            <BusinessNumberField register={register} errors={errors} trigger={trigger} />
+
+            <div className="mt-2.5 sm:mt-7.5">
+              <Button type="submit" varient="default" width="lg" height="md" fontSize="md">
+                회원가입
+              </Button>
+            </div>
+          </form>
+        </FormLayout>
+      </div>
+    </div>
+  );
+}
