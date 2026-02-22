5337c488ba7a434be8c2c9d8cdd477365226bcbd
lsohyuniil <lim07so10h@gmail.com>
2026-02-23 01:46:30 +0900

    페이지네이션 수정으로 인한 오류 부분 임시 주석 처리

--- app/mypage/reservations/components/ReservationContent.tsx
+++ app/mypage/reservations/components/ReservationContent.tsx
@@ -3,7 +3,7 @@
 import { CHILD_RESERVATION_TABS, OWNER_RESERVATION_TABS } from "../constants/reservationTabs";
 import FilteredTabSection from "@/components/feature/filtered-tab-section/FilteredTabSection";
 import { ReservationItem } from "../types";
-import DateGroupedPaginatedSection from "@/components/feature/date-grouped-pagination/DateGroupedPaginatedSection";
+// import DateGroupedPaginatedSection from "@/components/feature/date-grouped-pagination/DateGroupedPaginatedSection";
 import ReservationCard from "./ReservationCard";
 
 interface Props {
@@ -20,14 +20,15 @@ export default function ReservationContent({ items, role }: Props) {
       tabs={tabs}
       filterKey="status"
       render={filtered => (
-        <DateGroupedPaginatedSection
-          items={filtered}
-          getDate={item => item.date}
-          emptyText="예약 내역이 없습니다."
-          render={item => (
-            <ReservationCard key={item.id} {...item} role={item.role} status={item.status} />
-          )}
-        />
+        <></>
+        // <DateGroupedPaginatedSection
+        //   items={filtered}
+        //   getDate={item => item.date}
+        //   emptyText="예약 내역이 없습니다."
+        //   render={item => (
+        //     <ReservationCard key={item.id} {...item} role={item.role} status={item.status} />
+        //   )}
+        // />
       )}
     />
   );
--- app/mypage/sharing/history/components/HistoryContent.tsx
+++ app/mypage/sharing/history/components/HistoryContent.tsx
@@ -2,7 +2,7 @@
 
 import { SharingHistoryItem } from "../types";
 import FilteredTabSection from "@/components/feature/filtered-tab-section/FilteredTabSection";
-import DateGroupedPaginatedSection from "@/components/feature/date-grouped-pagination/DateGroupedPaginatedSection";
+// import DateGroupedPaginatedSection from "@/components/feature/date-grouped-pagination/DateGroupedPaginatedSection";
 import { OWNER_SHARING_HISTORY_TABS } from "../constants/historyTabs";
 import HistoryCard from "./HistoryCard";
 
@@ -17,12 +17,13 @@ export default function HistoryContent({ items }: Props) {
       tabs={OWNER_SHARING_HISTORY_TABS}
       filterKey="status"
       render={filtered => (
-        <DateGroupedPaginatedSection
-          items={filtered}
-          getDate={item => item.createdAt}
-          emptyText="나눔 내역이 없습니다."
-          render={item => <HistoryCard key={item.id} {...item} status={item.status} />}
-        />
+        <></>
+        // <DateGroupedPaginatedSection
+        //   items={filtered}
+        //   getDate={item => item.createdAt}
+        //   emptyText="나눔 내역이 없습니다."
+        //   render={item => <HistoryCard key={item.id} {...item} status={item.status} />}
+        // />
       )}
     />
   );
