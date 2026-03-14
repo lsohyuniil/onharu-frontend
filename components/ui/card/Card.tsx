5bacfc8d62ad101e5a5560097ec9eb3f118c7e1f
yeaseula <kminea@naver.com>
2026-03-14 17:01:15 +0900

    👽️ 가게별 review 호출 api 폴더 구조 병합

--- components/ui/card/Card.tsx
+++ components/ui/card/Card.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Like } from "../../feature/StoreLike";
+import LikeButton from "@/components/feature/LikeButton";
 import { CardProps } from "./type/type";
 import { cn } from "@/lib/utils";
 
@@ -45,6 +46,7 @@ export const Card = (props: CardProps) => {
         </div>
         <div className="relative flex-1 bg-white p-2.5 md:p-4">
           <div className="absolute top-2 right-5 z-5">
+            <LikeButton storeId={Number(props.storeId)} isLiked></LikeButton>
             <Like isLiked={false} />
           </div>
           <p className="md:text-md flex items-center gap-2 pr-6 text-base font-bold">
